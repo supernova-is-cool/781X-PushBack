bace28eef6d1f45a475265b26d1bff1a5cbe247d
Andrew Curtis <80860310+meisZWFLZ@users.noreply.github.com>
2026-02-28 21:02:14 -0700

    feat: :construction: tune sawp a bit

--- src/auton/autons/soloWinPoint.cpp
+++ src/auton/autons/soloWinPoint.cpp
@@ -24,11 +24,11 @@ void autons::soloWinPoint() {
   // === STARTING POSE ===
 
   // TODO: Determine good sawp starting pose
-  const Pose startingPosition = {-2 * TILE, -(TILE - DRIVE_LENGTH / 2) + 2,
+  const Pose startingPosition = {-2 * TILE, -TILE + DRIVE_LENGTH / 2 + 1,
                                  AUDIENCE};
   bot.setPose(startingPosition);
 
-  const Pose rightMatchloader = {MIN_X + DRIVE_LENGTH / 2, -2 * TILE,
+  const Pose rightMatchloader = {MIN_X + DRIVE_LENGTH / 2 + 3, -2 * TILE + 2,
                                  RED_STATION};
 
   bot.lateralPID.kP *= .75;
@@ -45,15 +45,15 @@ void autons::soloWinPoint() {
   bot.turnToHeading(RED_STATION, 1000);
   bot.waitUntilDone();
   // Move into matchloader to pick up blocks
-  bot.moveToX(rightMatchloader.x, 2000, {.maxSpeed = 50});
+  bot.moveToX(rightMatchloader.x, 1500, {.maxSpeed = 50});
   bot.waitUntilDone();
 
   // Give time to yoink the blocks
   bot.tank(10, 10);
-  pros::delay(700);
+  pros::delay(200);
   bot.tank(0, 0);
 
-  const Pose rightLongGoal = {-TILE - DRIVE_LENGTH / 2 + 4, -2 * TILE - 2,
+  const Pose rightLongGoal = {-TILE - DRIVE_LENGTH / 2 + 4, rightMatchloader.y,
                               RED_STATION};
 
   // Score 4 blocks on long goal
@@ -65,13 +65,13 @@ void autons::soloWinPoint() {
   bot.tank(-10, -10);
 
   bot.intake.goToTOP();
-  pros::delay(400);
-  bot.intake.goToOutaking();
-  pros::delay(200);
-  bot.intake.goToTOP();
+  // pros::delay(400);
+  // bot.intake.goToOutaking();
+  // pros::delay(200);
+  // bot.intake.goToTOP();
   pros::delay(2500);
-  bot.intake.goToOutaking();
-  pros::delay(200);
+  // bot.intake.goToOutaking();
+  // pros::delay(200);
   bot.intake.goToTOP();
 
   bot.intake.goToIdle();
@@ -79,7 +79,7 @@ void autons::soloWinPoint() {
   bot.tank(0, 0);
 
   const Pose rightCenterBallsTarget{
-      -TILE + 1.5 * BALL_INNER_DIAM - DRIVE_WIDTH / 2, -TILE};
+      -TILE + 1 * BALL_INNER_DIAM - DRIVE_WIDTH / 2, -TILE};
   const Pose leftCenterBallsTarget{rightCenterBallsTarget.x,
                                    -rightCenterBallsTarget.y};
   // Exit long goal
@@ -87,6 +87,7 @@ void autons::soloWinPoint() {
                    {.minSpeed = 48, .earlyExitRange = 30});
   // Scoop up the center balls
   bot.matchLoader.retract();
+  bot.intake.goToStoring();
   bot.moveToPoint(rightCenterBallsTarget, 3000,
                   {.maxSpeed = 60, .minSpeed = 40, .earlyExitRange = 6});
   bot.moveToPoint(leftCenterBallsTarget, 2000,
@@ -101,14 +102,14 @@ void autons::soloWinPoint() {
       100, 5000, true);
   bot.cancelMotion();
 
-  const float middleGoalTargetTheta = (RED_STATION + REFEREE) / 2;
+  const float middleGoalTargetTheta = -45;
   // Move to be directly in front of middle goal
   bot.moveToLine({0, 0, middleGoalTargetTheta}, 1500);
   bot.waitUntilDone();
 
   // Go into middle goal
   bot.turnToHeading(middleGoalTargetTheta, 1000);
-  const Pose middleGoalTarget = Pose::fromPolar(15, middleGoalTargetTheta);
+  const Pose middleGoalTarget = Pose::fromPolar(4, middleGoalTargetTheta);
   bot.moveToPoint(middleGoalTarget, 1000, {.forwards = false, .maxSpeed = 80});
   bot.waitUntilDone();
 
@@ -140,9 +141,12 @@ void autons::soloWinPoint() {
   bot.intake.goToStoring();
   bot.matchLoader.extend();
 
+  bot.angularPID.kP *= 1.25;
   // Face matchloader to prepare for scoring
   bot.turnToHeading(RED_STATION, 1000);
   bot.waitUntilDone();
+  bot.angularPID.kP /= 1.25;
+  
   // Move into matchloader to pick up blocks
   bot.moveToX(leftMatchloader.x, 1000, {.maxSpeed = 50});
   bot.waitUntilDone();
@@ -153,8 +157,7 @@ void autons::soloWinPoint() {
   bot.tank(0, 0);
 
   // Score on left long goal
-  const Pose leftLongGoal = {-TILE - DRIVE_LENGTH / 2 + 4, -2 * TILE - 2,
-                             RED_STATION};
+  const Pose leftLongGoal = {rightLongGoal.x, -rightLongGoal.y, RED_STATION};
 
   // Score 4 blocks on long goal
   bot.moveToPoint(leftLongGoal, 1500,
