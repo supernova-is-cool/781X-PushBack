178cbec2c9dbdc42d97b9aa97bc77e8e4f27f529
Andrew Curtis <80860310+meisZWFLZ@users.noreply.github.com>
2026-04-08 19:29:18 -0700

    refactor: :truck: relocate CustomIMU to robot.cpp

--- src/robot.cpp
+++ src/robot.cpp
@@ -7,7 +7,20 @@
 #include <cstdint>
 #include <memory>
 #include <vector>
-#include "subsystems/imu.hpp"
+
+class CustomIMU : public pros::IMU {
+  public:
+    CustomIMU(int port, double scalar)
+      : pros::IMU(port),
+        m_port(port),
+        m_scalar(scalar) {}
+    virtual double get_rotation() const override {
+      return pros::c::imu_get_rotation(m_port) * m_scalar;
+    }
+  private:
+    const int m_port;
+    const double m_scalar;
+};
 
 lemlib::Drivetrain robotConfig::makeDrivetrain() const {
   return {&this->motors.left,
