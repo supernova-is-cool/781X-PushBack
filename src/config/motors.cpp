a8cecddfbe8665f4a92e6f70463880a3076e9154
Andrew Curtis <atcurtis08@gmail.com>
2026-04-12 08:59:26 +0000

    fix: :bug: configure the lever as a red cartridge

--- src/config/motors.cpp
+++ src/config/motors.cpp
@@ -4,5 +4,5 @@ robotConfig::Motors robotConfig::Motors::motors{
     .left{11, -12, -14},
     .right{17, 19, -20},
     .intake{-1},
-    .lever{-21},
+    .lever{{-21}, pros::v5::MotorGears::red},
 };
