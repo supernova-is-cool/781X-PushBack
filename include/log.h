34f181d1a756c163c21ea12cc4ccfdc104da1c18
Andrew Curtis <80860310+meisZWFLZ@users.noreply.github.com>
2026-04-21 10:39:59 -0500

    fix: :technologist: enable control clicking on the file:line part of the log

--- include/log.h
+++ include/log.h
@@ -3,5 +3,5 @@
 #include "pros/rtos.hpp"
 #include <print>
 #define log(...)                                                               \
-  std::println("[{}]\t{}\t({}@{}:{})", pros::millis(),                         \
+  std::println("[{}]\t{}\t({}() @ {}:{})", pros::micros(),                     \
                std::format(__VA_ARGS__), __FUNCTION__, __FILE__, __LINE__)
